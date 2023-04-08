ec9f416fb203761aacb14d2f6125a917386ff780
Anthony Infortunio <contact@anthonyinfortun.io>
2023-04-08 16:50:00 -0400

    move query results to an external file to clean up code

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const {getPackageQuery} = require('../linux/getPackageQuery');
+const { getDebianResults, getArchResults, getUbuntuResults } = require('../linux/getQueryResults');
 var router = express.Router();
 //config results
 const resultsLimit = 30;
@@ -19,57 +20,21 @@ router.get('/search/:packageName', async(req,res,next)=>{
     I created individual variables for each rather than using an array because the each api could be called
     from and return results in different ways.
   */
-  let debianResults=[];
-  let archResults=[];
-  let ubuntuResults=[];
   //get debian package query results
-  try{
-    let response = await fetch(packageQuerys[0].url,{
-      method: "GET",
-    });
-    if (response.ok && response.headers.get('content-type').includes('application/json')){
-      debianResults = (await response.json()).results;
-    }
-  }catch(e){
-    console.log(`Error ${e} when fetching search results for package ${packageName} in the debian api`);
-    res.status(500).json({error: `Error when fetching search results for package ${packageName} in the debian api`})
-  }
+  let debianResults = await getDebianResults(packageName,packageQuerys,res);
   //get arch query results
-  try{
-    let response = await fetch(packageQuerys[1].url,{
-      method: "GET",
-    });
-    if (response.ok && response.headers.get('content-type').includes('application/json')){
-      archResults = (await response.json()).results;
-    }
-  }catch(e){
-    console.log(`Error ${e} when fetching search results for package ${packageName} in the arch api`);
-    res.status(500).json({error: `Error when fetching search results for package ${packageName} in the arch api`})
-  }
+  let archResults = await getArchResults(packageName,packageQuerys,res);
   //get ubuntu query results
-  try{
-    let response = await fetch(packageQuerys[2].url,{
-      method: "GET",
-    });
-    //if a match isn't found on ubuntu the server responds with an error so we have to check if it is in json format
-    if (response.ok && response.headers.get('content-type').includes('application/json')){
-      ubuntuResults = (await response.json());
-    }
-  }catch(e){
-    console.log(`Error ${e} when fetching search results for package ${packageName} in the ubuntu api`);
-    res.status(500).json({error: `Error when fetching search results for package ${packageName} in the ubuntu api`})
-  }
+  let ubuntuResults = await getUbuntuResults(packageName,packageQuerys,res);
   //limit results
-  debianResults.other = debianResults.other.splice(0,resultsLimit);
+  debianResults = debianResults.splice(0,resultsLimit);
   archResults = archResults.splice(0,resultsLimit-1);
   //ubuntu only shows the exact match for packages
   //return package results
   res.status(200).json({
-    packageResults: [
-      debianResults,
-      archResults,
-      ubuntuResults
-    ]
+    debian: debianResults,
+    arch: archResults,
+    ubuntu: ubuntuResults
   });
 })
 

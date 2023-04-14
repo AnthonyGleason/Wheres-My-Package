3bce2e44c84e0bfe5c8df6836f88fca0981d9374
Anthony Infortunio <contact@anthonyinfortun.io>
2023-04-14 15:29:06 -0400

    fix arch linux logo sizing

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,7 +4,7 @@ import '../styles/Nav.css';
 export default function Nav(){
   return(
     <div className='nav'>
-      <img src={archLogo} className='pointer' onClick={()=>{window.location.href='/Wheres-My-Package'}} />
+      <img src={archLogo} className=' arch-logo pointer' onClick={()=>{window.location.href='/Wheres-My-Package'}} />
       <ul className='nav-buttons'>
         <li className='pointer' onClick={()=>{window.location.href='/Wheres-My-Package'}}>Packages</li>
       </ul>

47c87a09725bead56d313a9618a1ac71e04a7ee8
Anthony Infortunio <contact@anthonyinfortun.io>
2023-09-06 11:56:42 -0400

    update footer css

--- client/src/Components/footer/Footer.tsx
+++ client/src/Components/footer/Footer.tsx
@@ -8,7 +8,6 @@ import heartImg from '../../Assets/heart.svg';
 export default function Footer(){
   return(
     <footer className='footer'>
-      <p>Made with <img src={heartImg} alt='a heart' /> by Anthony Infortunio</p>
       <section className='footer-img-container'>
         <img className='pointer' onClick={()=>{window.location.href='https://www.linkedin.com/in/anthony-infortunio-872645220/'}} src={linkedinLogo} alt='linkedin logo' />
         <img className='pointer' onClick={()=>{window.location.href='https://github.com/antinf'}} src={githubLogo} alt='github logo' />
